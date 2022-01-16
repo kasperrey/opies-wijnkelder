89f213438b45e5f33d7a3d5f7d26d842395ebbd2
Kasper Reynders <kasper.reynders@gmail.com>
2022-01-16 14:04:06 +0100

    methodes gemaakt

--- opies-weinkelder3.py
+++ opies-weinkelder3.py
@@ -11,60 +11,61 @@ from tkinter import *
 import pickle
 import time
 
-class Spel:
+class Doek:
     def __init__(self):
         self.tk = Tk()
         self.tk.title("opi's wijnkelder")
         self.tk.resizable(0, 0)
         self.canvas = Canvas(self.tk, width=500, height=500)
 
-def drink_een_biertje():
-    load_file = open('c://Users//Janick//Documents//biertjes.txt', 'rb')
-    biertjes = pickle.load(load_file)
-    load_file.close()
+def print_biertjes(biertjes):
     for key in biertjes:
         print( "{}:{}".format( key, biertjes[key] ))
+
+def load_biertjes():
+    load_file = open('c://Users//Janic//Documents//biertjes.txt', 'rb')
+    biertjes = pickle.load(load_file)
+    load_file.close()
+    return biertjes
+
+def save_biertjes(biertjes):
+    save_file = open('c://Users//Janic//Documents//biertjes.txt', 'wb')
+    pickle.dump(biertjes, save_file)
+    save_file.close()
+
+def drink_een_biertje():
+    biertjes = load_biertjes()
+    print_biertjes(biertjes)
     biertje_naam = input("welk biertje of wijntje ga je drinken?")
-    if biertjes[biertje_naam] < 0:
+    if biertjes[biertje_naam] > 0:
         biertjes[biertje_naam] -= 1
     else:
         print('je hebt geen %s meer' % (biertje_naam))
-    save_file = open('c://Users//Janick//Documents//biertjes.txt', 'wb')
-    pickle.dump(biertjes, save_file)
-    save_file.close()
+    print_biertjes(biertjes)
+    save_biertjes(biertjes)
     
 def zien():
-    load_file = open('c://Users//Janick//Documents//biertjes.txt', 'rb')
-    loaded_biertjes = pickle.load(load_file)
-    load_file.close()
-    for key in loaded_biertjes:
-        print( "{}:{}".format( key, loaded_biertjes[key] ))
+    print_biertjes(load_biertjes())
         
 def koop_een_biertje():
-    load_file = open('c://Users//Janick//Documents//biertjes.txt', 'rb')
-    biertjes = pickle.load(load_file)
-    load_file.close()
-    for key in biertjes:
-        print( "{}:{}".format( key, biertjes[key] ))
+    biertjes = load_biertjes()
+    print_biertjes(biertjes)
     gekocht_biertje = input('welk biertje of wijntje heb je gekocht?')
     if gekocht_biertje in biertjes.keys():
         biertjes[gekocht_biertje] = 1
     else:
         biertjes[gekocht_biertje] += 1
-    for key in biertjes:
-        print( "{}:{}".format( key, biertjes[key] ))
-    save_file = open('c://Users//Janick//Documents//biertjes.txt', 'wb')
-    pickle.dump(biertjes, save_file)
-    save_file.close
+    print_biertjes(biertjes)
+    save_biertjes(biertjes)
+    
+doek = Doek()
 
-spel = Spel()
-
-btn = Button(spel.tk, text="klik hier als je een biertje of wijntje drinkt", command=drink_een_biertje)
+btn = Button(doek.tk, text="klik hier als je een biertje of wijntje drinkt", command=drink_een_biertje)
 btn.pack()
 
-btn = Button(spel.tk, text="klik hier om te zien hoeveel biertjes en wijntjes je nog hebt", command=zien)
+btn = Button(doek.tk, text="klik hier om te zien hoeveel biertjes en wijntjes je nog hebt", command=zien)
 btn.pack()
 
-btn = Button(spel.tk, text="klik hier als je een biertje of wijntje koopt", command=koop_een_biertje)
+btn = Button(doek.tk, text="klik hier als je een biertje of wijntje koopt", command=koop_een_biertje)
 btn.pack()
 
